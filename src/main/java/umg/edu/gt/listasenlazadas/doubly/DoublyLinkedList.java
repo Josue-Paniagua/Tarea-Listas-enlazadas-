b9353c89bb75d8a5d6d922d6abe9dd153fca6813
Josue Paniagua <jpaniaguao@miumg.edu.gt>
2026-04-14 22:31:11 -0600

    Update: Actualice el metodo clean y countOccurence en doubly

--- src/main/java/umg/edu/gt/listasenlazadas/doubly/DoublyLinkedList.java
+++ src/main/java/umg/edu/gt/listasenlazadas/doubly/DoublyLinkedList.java
@@ -90,8 +90,17 @@ public class DoublyLinkedList <T> {
 	     * @return cantidad de ocurrencias
 	     */
 	    public int countOccurrences(T value) {
-	        throw new UnsupportedOperationException(
-	                "TODO RETO: Implementar countOccurrences(T value) en DoublyLinkedList.");
+	       int count = 0;
+	       DoublyNode<T> current = head;
+	       
+	       while (current != null) {
+	    	   if(isSameValue(current.getValue(),value)) {
+	    		   count ++;
+	    	   }
+	    	    current = current.getNext();
+	       }
+	       return count;
+	       
 	    }
 
 	    /**
@@ -101,8 +110,21 @@ public class DoublyLinkedList <T> {
 	     * @return nodos eliminados
 	     */
 	    public int clean() {
-	        throw new UnsupportedOperationException(
-	                "TODO RETO: Implementar clean() en DoublyLinkedList.");
+	       int count = 0;
+	       DoublyNode<T> current = head;
+	       
+	       while (current != null) {
+	    	   
+	    	   DoublyNode<T> next = current.getNext();
+	    	   current.setNext(null);
+	    	   current = next;
+	    	   count ++;
+	       }
+	       head = null;
+	       tail = null;
+	       size = 0;
+	       return count;
+	    		
 	    }
 
 	    /**
